92910796c58d8f654070389972e1705f2320c2d9
Shivaraj Deyannavar <62126060+shivubd@users.noreply.github.com>
2021-06-08 19:45:41 +0530

    Add files via upload

---
+++ IoDemo1.java
@@ -0,0 +1,15 @@
+//use of println(),print()
+class IoDemo1{
+	public static void main(String args[]){
+		int x=10;
+		double y=20.0;
+		char c='X';
+		System.out.println("1.println");
+		System.out.println("2.println");
+		System.out.print("1.print");
+		System.out.print(" 2.print");
+		System.out.printf("x= %d  y=%20.2f  c=%c",x,y,c);
+	}
+}
+	
+	
